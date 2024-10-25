04a858f1bd31b4411ec58933f2955eb6545c73da
Jack Mao <maosf@hotmail.com>
2024-10-25 11:21:51 -0400

    add interval

--- src/App.js
+++ src/App.js
@@ -1,45 +1,49 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import RoomTemp from "./RoomTemp";
-import TempAdjust from "./TempAdjust";
-import PopupForm from "./PopupForm";
+//import TempAdjust from "./TempAdjust";
+//import PopupForm from "./PopupForm";
 function App() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  // const loadData = async () => {
-  //   try {
-  //     const response = await fetch("http://192.168.68.65:3001/api");
-  //     const data = await response.json();
-  //     if (data) {
-  //       setData(data);
-  //       console.log(data);
-  //     }
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-  useEffect(() => {
-    fetch("http://192.168.68.65:3001/api")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("network response is not ok");
-        }
-        return response.json();
-      })
-      .then((data) => {
+  const loadData = async () => {
+    try {
+      const response = await fetch("http://192.168.68.65:3001/api");
+      const data = await response.json();
+      if (data) {
         setData(data);
         setLoading(false);
         console.log(data);
-      })
-      .catch((error) => {
-        setError(error.message);
-        setLoading(false);
-      });
-    // const interval = setInterval(() => {
-    //   loadData();
-    // }, 6000);
-    // return () => clearInterval(interval);
+      }
+    } catch (err) {
+      setError(err.message);
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    // fetch("http://192.168.68.65:3001/api")
+    //   .then((response) => {
+    //     if (!response.ok) {
+    //       throw new Error("network response is not ok");
+    //     }
+    //     return response.json();
+    //   })
+    //   .then((data) => {
+    //     setData(data);
+    //     setLoading(false);
+    //     console.log(data);
+    //   })
+    //   .catch((error) => {
+    //     setError(error.message);
+    //     setLoading(false);
+    //   });
+    // loadData();
+    //update temp every 6 seconds
+    const interval = setInterval(() => {
+      loadData();
+    }, 6000);
+    return () => clearInterval(interval);
   }, []);
   const adjustTemp = (tempSetting) => {
     console.log(tempSetting);
@@ -65,11 +69,7 @@ function App() {
   if (error) return <div>Error: {error} </div>;
   return (
     <div className="App">
-      {/* <h1>The room temperature</h1> */}
-      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
-      {/* <TempAdjust onAdjust={adjustTemp} /> */}
       <RoomTemp data={data} onAdjust={adjustTemp} />
-      {/* <PopupForm onAdjust={adjustTemp} /> */}
     </div>
   );
 }
